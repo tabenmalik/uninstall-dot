3c425d01d52570c2ec19b4274c568f55a4f26c51
Taben Malik <tabenjmalik@gmail.com>
2026-01-16 17:12:31 -0600

    quick test set

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,61 @@
 import shutil
+from unittest.mock import MagicMock
+import sys
+import os
 
-from uninstall_dot._main import _main
+import pytest
+
+from uninstall_dot import _main
 
 def test_install():
     assert shutil.which("uninstall-dot") is not None
+
+
+@pytest.mark.parametrize("cmd", [
+    ("pip", "uninstall", "numpy"),
+    ("uninstall-dot", "install", "numpy"),
+    ("pip", "help"),
+    ("hello", "freeze", "--all"),
+])
+def test_passthrough(monkeypatch, cmd):
+    mock_exec = MagicMock()
+    monkeypatch.setattr(_main.sys, "argv", cmd)
+    monkeypatch.setattr(_main, "execvp", mock_exec)
+    _main._main()
+    assert mock_exec.called
+    assert mock_exec.call_args == (("pip", ["pip", *cmd[1:]]),)
+
+
+def test_uninstall_no_toml(monkeypatch, tmp_path):
+    # directory path doesn't get converted to project name without a pyproject.toml
+    cmd = ["uninstall-dot", "uninstall", "."]
+    mock_exec = MagicMock()
+    monkeypatch.setattr(_main.sys, "argv", cmd)
+    monkeypatch.setattr(_main, "execvp", mock_exec)
+    monkeypatch.chdir(tmp_path)
+
+    _main._main()
+    assert mock_exec.called
+    assert mock_exec.call_args == (("pip", ["pip", "uninstall", "."]),)
+
+
+def test_uninstall_with_toml(monkeypatch, tmp_path):
+    # look up project name in pyproject.toml
+    cmd = ["uninstall-dot", "uninstall", str(tmp_path)]
+    mock_exec = MagicMock()
+    monkeypatch.setattr(_main.sys, "argv", cmd)
+    monkeypatch.setattr(_main, "execvp", mock_exec)
+
+    with open(tmp_path / "pyproject.toml", mode="wb") as fobj:
+        fobj.write(
+            b"[build-system]\n"
+            b"requires = [\"setuptools>=61.0\"]\n"
+            b"build-backend = \"setuptools.build_meta\"\n"
+            b"\n"
+            b"[project]\n"
+            b"name = \"fizzbuzz\"\n"
+        )
+
+    _main._main()
+    assert mock_exec.called
+    assert mock_exec.call_args == (("pip", ["pip", "uninstall", "fizzbuzz"]),)
